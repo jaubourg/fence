f71128afc70cbfd84451ea89c3035e1bc27a8caf
jaubourg <j@ubourg.net>
2012-05-07 02:26:42 +0200

    Re-organizes sample tester.

--- sample/index.js
+++ sample/index.js
@@ -15,49 +15,77 @@ fs.readdir( subdir, function( error, files ) {
 	}
 
 	var results = [],
-		typeLength = 0,
-		methods = files.map(function( name ) {
+		types = files.map(function( name ) {
 			var fn = require( path.join( subdir, name ) ),
-				type = name.replace( /\.js$/, "" ),
-				now, tmp;
-			if ( type.length > typeLength ) {
-				typeLength = type.length;
-			}
-			return function loop( next, it ) {
-				if ( !( --it ) ) {
-					results.push({
-						time: +new Date - now,
-						type: type
-					});
-					return next();
-				}
-				if ( !now ) {
-					now = +new Date;
-				}
-				function done( result ) {
-					loop( next, it );
-				}
-				if (( tmp = fn( parent, done ) ) && tmp.done ) {
-					tmp.done( done );
-				}
-			};
+				object = {
+					type: name.replace( /\.js$/, "" ),
+					time: 0,
+					run: function( callback ) {
+						function done() {
+							object.time += ( +new Date - now );
+							object.result = [].slice.call( arguments );
+							callback();
+						}
+						now = +new Date;
+						if (( tmp = fn( parent, done ) ) && tmp.done ) {
+							tmp.done( done );
+						}
+					}
+				};
+			return object;
 		}),
-		spaces = ( new Array( typeLength ) ).join( " " );
+		total = 12,
+		loops = total * types.length,
+		iteration = 0,
+		count = 0;
+
+	function TableLog() {
+		var width = [],
+			lines = [],
+			slice = lines.slice;
+		return {
+			line: function() {
+				var cols = slice.call( arguments );
+				cols = cols.map(function( value, i ) {
+					value = value.trim();
+					width[ i ] = width[ i ] || 0;
+					if ( value.length > width[ i ] ) {
+						width[ i ] = value.length;
+					}
+					return value;
+				});
+				lines.push( cols );
+			},
+			flush: function() {
+				lines.forEach(function( cols ) {
+					console.log( cols.map(function( value, i ) {
+						return value + ( new Array( width[ i ] - value.length + 1 ) ).join( " " );
+					}).join( " | " ) );
+				});
+				lines = [];
+				width = [];
+			}
+		};
+	}
 
 	(function next() {
-		var fn = methods.shift();
-		if ( fn ) {
-			fn( next, 5 );
+		if ( iteration < loops ) {
+			var typeId = iteration++ % types.length;
+			if ( !typeId ) {
+				count++;
+				console.log( "iteration " + count + " / "  + total );
+			}
+			types[ typeId ].run( next );
 		} else {
-			results.sort(function( a, b ) {
+			types.sort(function( a, b ) {
 				return a.time - b.time;
 			});
-			results.forEach(function( data ) {
-				console.log( data.type +
-					( spaces.substr( 0, typeLength - data.type.length ) ) +
-					" | (ms) " + Math.round( data.time / 5 ) +
-					" | " + Math.round( data.time * 100 / results[ 0 ].time ) + "%" );
+			console.log( "\n----- RESULTS -----\n" );
+			var logger = TableLog();
+			types.forEach(function( data ) {
+				logger.line( data.type, "(ms) " + Math.round( data.time / total ), Math.round( data.time * 100 / types[ 0 ].time ) + "%" );
 			});
+			logger.flush();
 		}
 	})();
 });
